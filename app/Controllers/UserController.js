05e5fefca0fbcc0459a66314d5909c56814e5f26
LAPTOP-H06CCH7C\santi <santiago_ortiz_soto363@outlook.com>
2024-12-01 14:05:02 -0600

    La clase UserController fue implementada de manera correcta y se creo un nuevo archivo userHandler

--- app/Controllers/UserController.js
+++ app/Controllers/UserController.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const dataHandlerUser = require('../Handlers/userHandler')
+let messageInfo;
 //email, name, password, career, placeOfOrigin
 class UserController
 {
@@ -16,13 +17,13 @@ class UserController
                 return res.status(400).json({message:"Faltan Campos obligatorios"});
             }
     
-            const newUser = await dataHandlerUser.createUser(user);
+            messageInfo = await dataHandlerUser.createUser(user);
             req.session.userId = newUser.idUser;
 
-            res.status(201).json({message: "Usuario creado con éxito", user: newUser });
+            res.status(201).json({message: messageInfo });
         }
         catch(error) {
-            res.status(500).json({ message: "Error interno del servidor creacion de usuario nuevo" });
+            res.status(500).json({ message: error.message });
         }
     }
 
@@ -31,11 +32,11 @@ class UserController
             const idProject = req.params.idProject;
             const userId = req.session.userId;
 
-            const result = await dataHandlerUser.sendRequest(idProject, userId);
-            res.status(201).json({ message: 'La solicitud fue enviada', result });
+            messageInfo = await dataHandlerUser.sendRequest(idProject, userId);
+            res.status(201).json({ message: result });
         } catch (error) {
             console.error("Error en sendRequest:", error);
-            res.status(500).json({ message: "Error interno del servidor enviando solicitud" });
+            res.status(500).json({ message: error.message });
         }
     }
 
@@ -43,11 +44,11 @@ class UserController
         try {
             const user = req.body;
     
-            const updatedUser = await dataHandlerUser.updateUser(user);
-            res.status(200).json({ message: "Usuario actualizado con éxito", user: updatedUser });
+            messageInfo = await dataHandlerUser.updateUser(user);
+            res.status(200).json({ message: messageInfo });
         } catch (error) {
             console.error("Error en updateUser:", error);
-            res.status(500).json({ message: "Error interno del servidor actualizando usuario" });
+            res.status(500).json({ message: error.message });
         }
     }
     
@@ -58,21 +59,24 @@ class UserController
             const idProject = req.params.idProject;
             const userId = req.session.userId;
     
-            await dataHandlerUser.sendComment(idProject, userId, comment.message);
-            res.status(201).json({ message: 'Comentario creado con éxito' });
+            messageInfo = await dataHandlerUser.sendComment(idProject, userId, comment.message);
+            res.status(201).json({ message: messageInfo });
         } catch (error) {
             console.error("Error en createComment:", error);
-            res.status(500).json({ message: "Error interno del servidor creando comentario" });
+            res.status(500).json({ message: error.message });
         }
     }
     
 
     static async leaveProject (req,res) {
         try {
-
+            const userId = req.session.userId;
+            const idProject = req.params.idProject;
+            messageInfo = await dataHandlerUser.quitProject(idProject,userId);
+            res.status(201).json({ message: messageInfo });
         }
         catch(error) {
-            res.status(500).json({ message: "Error interno del servidor darse de baja de proyecto" });
+            res.status(500).json({ message: error.message });
         }
     }
 
